80ec49a7beb23f02e9bbeef5ed3c45a5d0b9540d
Jordy Manner <jordy.manner@milkcreation.fr>
2020-12-12 16:08:39 +0000

    docs v1.0

---
+++ README.md
@@ -0,0 +1,41 @@
+# Pollen Solutions - Docs Component
+
+[![Latest Version](https://img.shields.io/badge/release-1.0.0-blue?style=for-the-badge)](https://www.pollen-solutions/components/docs/)
+[![MIT Licensed](https://img.shields.io/badge/license-MIT-green?style=for-the-badge)](LICENSE.md)
+
+**Docs** Component.
+
+## Installation
+
+```bash
+composer require pollen-solutions/docs
+```
+
+## Setup
+
+### Framework Declaration
+
+In config/app.php file.
+
+```php
+return [
+      //...
+      'providers' => [
+          //...
+          \Pollen\Docs\DocsServiceProvider::class,
+          //...
+      ];
+      // ...
+];
+```
+### Framework Configuration
+
+```php
+// config/docs.php
+// @see /vendor/pollen-solutions/docs/resources/config/docs.php
+return [
+      //...
+
+      // ...
+];
+```
---
+++ src/Docs.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+
+namespace Pollen\Docs;
+
+use RuntimeException;
+use Pollen\Docs\Contracts\Docs as DocsContract;
+use Psr\Container\ContainerInterface as Container;
+use tiFy\Support\Concerns\BootableTrait;
+use tiFy\Support\Concerns\ContainerAwareTrait;
+use tiFy\Support\ParamsBag;
+
+class Docs implements DocsContract
+{
+    use BootableTrait, ContainerAwareTrait;
+
+    /**
+     * Instance de la classe.
+     * @var static|null
+     */
+    private static $instance;
+
+    /**
+     * Instance du gestionnaire de configuration.
+     * @var ParamsBag
+     */
+    private $configBag;
+
+    /**
+     * @param array $config
+     * @param Container|null $container
+     *
+     * @return void
+     */
+    public function __construct(array $config = [], Container $container = null)
+    {
+        $this->setConfig($config);
+
+        if (!is_null($container)) {
+            $this->setContainer($container);
+        }
+
+        if (!self::$instance instanceof static) {
+            self::$instance = $this;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function instance(): DocsContract
+    {
+        if (self::$instance instanceof self) {
+            return self::$instance;
+        }
+
+        throw new RuntimeException(sprintf('Unavailable %s instance', __CLASS__));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): DocsContract
+    {
+        if (!$this->isBooted()) {
+            $this->setBooted();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function config($key = null, $default = null)
+    {
+        if ($this->configBag === null) {
+            $this->configBag = new ParamsBag();
+        }
+
+        if (is_string($key)) {
+            return $this->configBag->get($key, $default);
+        } elseif (is_array($key)) {
+            return $this->configBag->set($key);
+        } else {
+            return $this->configBag;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setConfig(array $attrs): DocsContract
+    {
+        $this->config($attrs);
+
+        return $this;
+    }
+}

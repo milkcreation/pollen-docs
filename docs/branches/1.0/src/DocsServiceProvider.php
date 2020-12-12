7b320ad6316452f22311c0f3689383a6d838e0a6
mast3rm1lk <mast3rm1lk@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2020-12-12 16:54:13 +0000

    docs 1.0.x-dev

---
+++ docs/branches/1.0/src/DocsServiceProvider.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Pollen\Docs;
+
+use Pollen\Docs\Contracts\Docs as DocsContract;
+use tiFy\Container\ServiceProvider as BaseServiceProvider;
+
+class DocsServiceProvider extends BaseServiceProvider
+{
+    /**
+     * @var string[]
+     */
+    protected $provides = [
+        DocsContract::class
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function boot()
+    {
+        events()->listen('wp.booted', function () {
+            $this->getContainer()->get(DocsContract::class)->boot();
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function register(): void
+    {
+        $this->getContainer()->share(DocsContract::class, function (): DocsContract {
+            return new Docs(config('docs', []), $this->getContainer());
+        });
+    }
+}
---
+++ docs/branches/1.0/tests/DocsTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Pollen\Docs\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DocsTest extends TestCase
+{
+
+}
